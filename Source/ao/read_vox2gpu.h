1851bd14e596d656e4df5b9061da6995102d9ec3
Calina Copos <calinacopos@localhost.localdomain>
2012-03-14 01:54:21 -0700

    Work distributed on GPU - no idea if it works~

--- Source/ao/read_vox2gpu.h
+++ Source/ao/read_vox2gpu.h
@@ -4,8 +4,7 @@
 #include <cuda_runtime_api.h>
 
 extern "C" {
-  void run_kernel(int width, int height, unsigned int num_layers,
-        unsigned int mem_size, float *voxel_data, float *voxel_odata);
+  void run_kernel(int width, int height, int depth, int w, int h, int d, unsigned int mem_size, unsigned int mem_size_a, unsigned int mem_size_b, float *a, float *b, float *voxel_data, float *voxel_odata);
 };
 
 #endif
