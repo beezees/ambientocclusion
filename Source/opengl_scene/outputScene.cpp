fff41a59ba60cfbd09043a696f5870ef632c9f12
Afton Geil <freethehippos@gmail.com>
2012-03-18 17:46:47 -0700

    outputting an image from AO info, but it is very buggy

--- Source/opengl_scene/outputScene.cpp
+++ Source/opengl_scene/outputScene.cpp
@@ -8,7 +8,6 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <png.h>
 
 using namespace std;
 
@@ -25,9 +24,9 @@ GLMmodel* model;
 //Number of divisions in each direction for voxel grid:
 int grid = 256;
 
-float zcount;
-
 float objxMin, objxMax, objyMin, objyMax, objzMin, objzMax;
+
+GLfloat AOMin, AOMax;
 
 void init(void)
 {
@@ -45,32 +44,36 @@ void init(void)
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT);
 }
 
-void convertToObj(int x, int y, double obj[])
+void convertToWindow(GLfloat obj[], GLfloat win[])
 {
         int viewport[4];
         double modelview[16];
         double projection[16];
-        float z;
         double objx, objy, objz;
+	double winx, winy, winz;
+
+        objx = obj[0];
+        objy = obj[1];
+        objz = obj[2];
 
         glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
         glGetDoublev( GL_PROJECTION_MATRIX, projection );
         glGetIntegerv( GL_VIEWPORT, viewport );
 
-        glReadPixels( x, viewport[3]-y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &z);
-        gluUnProject( x, viewport[3]-y, z, modelview, projection, viewport, &objx, &objy, &objz);
+        gluProject( objx, objy, objz, modelview, projection, viewport, &winx, &winy, &winz);
 
-        obj[0] = objx;
-        obj[1] = objy;
-        obj[2] = objz;
+	win[0] = winx;
+	win[1] = winy;
+	win[2] = winz;
 
 	return;
 }
 
-void convertToVoxel(double objx, double objy, double objz, int vox[])
+
+void convertToObj(GLfloat vox[], GLfloat obj[])
 {
 //	double voxSize;
-	double voxSizex, voxSizey, voxSizez;
+	GLfloat voxSizex, voxSizey, voxSizez;
 
 	//Find dimensions of a single voxel:
 //	voxSize = 2.0/grid;
@@ -78,21 +81,65 @@ void convertToVoxel(double objx, double objy, double objz, int vox[])
 	voxSizey = (objyMax - objyMin)/grid;
 	voxSizez = (objzMax - objzMin)/grid;
 
-	//Coordinates of containing voxel:
-/*	vox[0] = floor((objx + 1.0f)/voxSize);
-	if (vox[0] == 256) vox[0] = 255;
-	vox[1] = floor((objy + 1.0f)/voxSize);
-        if (vox[1] == 256) vox[1] = 255;
-	vox[2] = floor((objz + 1.0f)/voxSize);
-        if (vox[2] == 256) vox[2] = 255;
-*/	vox[0] = floor((objx - objxMin)/voxSizex);
-	if (vox[0] == 256) vox[0] = 255;
-	vox[1] = floor((objy - objyMin)/voxSizey);
-        if (vox[1] == 256) vox[1] = 255;
-	vox[2] = floor((objz - objzMin)/voxSizez);
-        if (vox[2] == 256) vox[2] = 255;
+	//Coordinates in object space:
+	obj[0] = vox[0] * voxSizex - 1;
+	obj[1] = vox[1] * voxSizey - 1;
+	obj[2] = vox[2] * voxSizez - 1;
 
 	return;
+}
+
+int intArrayMax(int array[], int numElements)
+{
+        int max = 0;
+        int m;
+        for (m = 0; m < numElements; m++){
+                if (array[m] > max){
+                        max = array[m];
+                }
+        }
+        return max;
+}
+
+int intArrayMin(int array[], int numElements)
+{
+        int min = sizeof(int);
+        int n;
+        for (n = 0; n < numElements; n++){
+                if (array[n] < min){
+                        min = array[n];
+                }
+        }
+        return min;
+}
+
+void findMinMaxAO(void)
+{
+
+        string str;
+        double item;
+        int *ao;
+	ao = new int [winWidth*winHeight];
+        ifstream results;
+        results.open("nonzero_all.dat", ios::in);
+        int coordIndex = 0;
+	int i = 0;
+        while (!results.eof()){
+                getline(results, str);
+                istringstream results_iss(str);
+                while(results_iss >> item){
+                        coordIndex++;
+                        if (coordIndex % 4 == 0){
+                                ao[i] = item;
+				i++;
+                        }
+                }
+	}
+
+	AOMin = intArrayMin(ao, i);
+	AOMax = intArrayMax(ao, i);
+
+	delete [] ao;
 }
 
 void display(void)
@@ -105,18 +152,26 @@ void display(void)
 
 	glMatrixMode(GL_MODELVIEW);	//Matrix describing transformations
 	glLoadIdentity();
-	gluLookAt(0, 0, 4, 0, 0, 0, 0, 1, 0);	//point to look from, at, upward direction
+	gluLookAt(0, 0, 2, 0, 0, 0, 0, 1, 0);	//point to look from, at, upward direction
 
-	glCallList(displayList);
+//	glCallList(displayList);
 
 	glFlush();
 
-	int vox[3];
-	double obj[3];
+	findMinMaxAO();
+
+	string str;
+	GLfloat item;
+	GLfloat vox[3];
+	GLfloat obj[3];
+	GLfloat objx, objy, objz;
+	GLfloat win[3];
+	GLfloat rawAO;
+	GLfloat ao;
+	GLfloat oldColour;
         ifstream results;
-        results.open("nonzeroc.dat", ios::in);
+        results.open("nonzero_all.dat", ios::in);
         int coordIndex = 0;
-        int xin, yin, zin;
         while (!results.eof()){
                 getline(results, str);
                 istringstream results_iss(str);
@@ -124,59 +179,49 @@ void display(void)
                         coordIndex++;
                         if (coordIndex % 4 == 1) {
                                 vox[0] = item;
-//                              cout << item << ' ';
+//				cout << item << endl;
                         }
                         if (coordIndex % 4 == 2) {
                                 vox[1] = item;
-//                              cout << item << ' ';
+//				cout << item << endl;
                         }
                         if (coordIndex % 4 == 3){
                                 vox[2] = item;
-//                              cout << item << "\n";
+//				cout << item << endl;
                         }
 			if (coordIndex % 4 == 0){
-				ao = item;
-				cout << item << "\n";
+				rawAO = item;
+//				cout << item << endl;
 			}
-			convertToObj(vox[], obj[]);
-			objx = obj[0];
-			objy = obj[1];
-			objz = obj[2];		
-                }
+		}
+		ao = (rawAO - AOMin)/ (AOMax - AOMin);	//normalize AO values
+/*		cout << vox[0] << " "; 
+		cout << vox[1] << " "; 
+		cout << vox[2] << " ";
+		cout << ao << endl;
+*/		convertToObj(vox, obj);
+		objx = obj[0];
+		objy = obj[1];
+		objz = obj[2];
+/*		cout << objx << " " << objy << " ";
+		cout << objz << endl;
+*///		GLfloat colour[] = {0, ao * 255.0f, ao * 255.0f};	//scale AO for 8-bit RGB colour
+//		cout << vox[0] << " "; 
+		convertToWindow(obj, win);
+//		cout << win[0] << " " << win[1] << " " << win[2] << endl;
+		glReadPixels(win[0], win[1], 1, 1, GL_RGB, GL_FLOAT, &oldColour);
+//		cout << oldColour << "R " << *(&oldColour + 1) << "G " << *(&oldColour + 2) << "B ";
+		GLfloat colour[3];
+		colour[0] = oldColour - ao;
+		colour[1] = *(&oldColour) - ao;
+		colour[2] = *(&oldColour) - ao;
+		glRasterPos3fv(obj);	//set the object space coordinate to occlude
+		glDrawPixels(1, 1, GL_RGB, GL_FLOAT, colour);
+//		glWindowPos3fv(win[0], win[1], win[2]);		//set the window position of the occuded pixel (need to update GL or get extra code for this)
         }
         results.close();
 
-	//Convert from window location to closest voxel vertex location:
-        int pixelX, pixelY;
-	double obj[3];
-        double objx, objy, objz;
-        int vox[3];
-        int voxX, voxY, voxZ;
-	double depth[winWidth*winHeight];
-	ofstream filledVox;
-        filledVox.open("filledVox.dat", ios::out);
-        for (pixelY = 1; pixelY < winHeight; pixelY++){
-		for (pixelX = 0; pixelX < winWidth; pixelX++){
-        		convertToObj(pixelX, pixelY, obj);
-			zcount++;
-        		objx = obj[0];
-		        objy = obj[1];
-		        objz = obj[2];
-			depth[pixelX+pixelY*winWidth] = objz;
-//			cout << objz << endl;
-			if (objz >= -1.0f && objz <= 1.0f){
-				//TODO: find closest vertex and return normal to write to file
-//				filledVox << objx << " " << objy << " " << objz << endl;
-		        	convertToVoxel(objx, objy, objz, vox);
-		        	voxX = vox[0];
-		        	voxY = vox[1];
-		        	voxZ = vox[2];
-				filledVox << voxX << " " << voxY << " " << voxZ << endl;
-			}
-		}
-	}
-	filledVox.close();
-	cout << zcount;
+	glFlush();
 
 	cout << "done";
 	glutSwapBuffers();
@@ -186,23 +231,23 @@ void display(void)
 
 GLfloat arrayMax(GLfloat array[], GLuint numElements)
 {
-	GLfloat max = 0;
-	GLuint i;
-	for (i = 0; i < numElements; i++){
-		if (array[i] > max){
-			max = array[i];
-		}
-	}
-	return max;
+        GLfloat max = 0;
+        GLuint m;
+        for (m = 0; m < numElements; m++){
+                if (array[m] > max){
+                        max = array[m];
+                }
+        }
+        return max;
 }
 
 GLfloat arrayMin(GLfloat array[], GLuint numElements)
 {
         GLfloat min = sizeof(GLfloat);
-        GLuint i;
-        for (i = 0; i < numElements; i++){
-                if (array[i] < min){
-                        min = array[i];
+        GLuint n;
+        for (n = 0; n < numElements; n++){
+                if (array[n] < min){
+                        min = array[n];
                 }
         }
         return min;
@@ -269,48 +314,11 @@ int main(int argc, char** argv)
 		glmList(model, GLM_SMOOTH);
 	glEndList();
 
-/*	//Open file containing filled voxels and write to array:
-	string str;
-	string coord;
-	int item;
-	int (*voxels)[256][256];	//matrix of all possible voxel locations containing their status (empty = 0, filled =1)
-	voxels = new int[256][256][256];
-	ifstream vox_coords;
-	vox_coords.open("vox_coords.dat", ios::in);
-	int coordIndex = 0;
-	int xin, yin, zin;
-	while (!vox_coords.eof()){
-		getline(vox_coords, str);
-		istringstream coord_iss(str);
-		while(coord_iss >> item){
-			coordIndex++;
-			if (coordIndex % 3 == 1) {
-				xin = item;
-//				cout << item << ' ';
-			}
-			if (coordIndex % 3 == 2) {
-				yin = item;
-//				cout << item << ' ';
-			}
-			if (coordIndex % 3 == 0){
-				zin = item;
-				voxels[xin][yin][zin] = 1;
-//				cout << item << "\n";
-			}
-		}
-	}
-	vox_coords.close();
-	int test = voxels[0][145][130];
-	cout << test << '\n';
-	test = voxels[6][179][145];
-	cout << test << '\n';
-*/
 	//Callback functions:
 	glutDisplayFunc(display);
 	glutMainLoop();
 
 	glmDelete(model);
-//	delete [] voxels;
 
 	return 0;
 }
